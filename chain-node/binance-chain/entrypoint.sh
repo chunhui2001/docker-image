a1faa5823cc453f023b32659443dd1a2ff5992ec
zhangchunhui <zhangchunhui@hnnewsoft.com>
2021-08-29 13:09:51 +0800

    a
    a

--- chain-node/binance-chain/entrypoint.sh
+++ chain-node/binance-chain/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-geth --config /usr/local/binance-chain/mainnet/config.toml --datadir /usr/local/binance-chain/mainnet_node --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0 --nodiscover
+# geth --config /binance-chain/config.toml --datadir /binance-chain/mainnet_node --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0 --nodiscover
+cd /binance-chain && 
+geth --config config.toml --datadir ./node  --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0
