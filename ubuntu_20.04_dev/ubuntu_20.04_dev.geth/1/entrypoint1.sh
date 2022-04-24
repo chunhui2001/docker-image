77ba844d3fea945721c5c8923e5afd1200d9a0a9
zhangchunhui <zhangchunhui@hnnewsoft.com>
2022-04-24 19:29:52 +0800

    a
    a

--- ubuntu_20.04_dev/ubuntu_20.04_dev.geth/1/entrypoint1.sh
+++ ubuntu_20.04_dev/ubuntu_20.04_dev.geth/1/entrypoint1.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-geth --datadir /root/blkchain1 --verbosity 4 --nodiscover \
+geth --allow-insecure-unlock \
+ 	 --datadir /root/blkchain1 --verbosity 4 --nodiscover \
 	 --port 30303 \
 	 --nat=extip:172.16.197.179 \
-	 --http.addr "0.0.0.0" --http.port 8545 \
-	 --syncmode "full" -cache=4096 --http --metrics --maxpeers 30 
+	 --http.addr "0.0.0.0" --http.corsdomain "*" --http.port 8545 \
+	 --http.api 'personal,eth,net,web3,txpool,miner' \
+	 --syncmode "full" -cache=4096 --http --metrics --maxpeers 30 console
+
+# geth --datadir /root/blkchain1 --verbosity 4 --nodiscover \
+# 	 --port 30303 \
+# 	 --nat=extip:172.16.197.179 \
+# 	 --http.addr "0.0.0.0" --http.port 8545 \
+# 	 --http.api 'personal,eth,net,web3,txpool,miner' \
+# 	 --mine --miner.etherbase=YOUR_ETHEREUM_ADDRESS_HERE \
+# 	 --syncmode "full" -cache=4096 --http --metrics --maxpeers 30 console
--- ubuntu_20.04_dev/ubuntu_20.04_dev.geth/2/entrypoint2.sh
+++ ubuntu_20.04_dev/ubuntu_20.04_dev.geth/2/entrypoint2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-geth --datadir /root/blkchain2 --verbosity 4 --nodiscover \
+geth --allow-insecure-unlock \
+ 	--datadir /root/blkchain2 --verbosity 4 --nodiscover \
      --port 30303 \
 	--nat=extip:172.16.197.180 \
-	--http.addr "0.0.0.0" --http.port 8546 \
-	--syncmode "full" -cache=4096 --http --metrics --maxpeers 30
+	 --http.addr "0.0.0.0" --http.corsdomain "*" --http.port 8545 \
+	 --http.api 'personal,eth,net,web3,txpool,miner' \
+	--syncmode "full" -cache=4096 --http --metrics --maxpeers 30 console
 
